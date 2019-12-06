59b2eaa1d0606656a53c26c6eb4e26074cca2cd2
Corom Thompson <coromt@microsoft.com>
2019-12-06 15:16:57 -0800

    Adding support for blob indexers and direct import of json files

--- Program.cs
+++ Program.cs
@@ -1,14 +1,17 @@
 ﻿using Microsoft.Azure.Search;
 using Microsoft.Azure.Search.Models;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace IdxPart
@@ -21,6 +24,9 @@ namespace IdxPart
         private static string _searchServiceEndpoint;
         private static string indexerTemplate;
         private static string datasourceTemplate;
+        private static string _apiKey;
+        private static int _batchCounter;
+        private static long _durationMS;
 
         public static void Main(string[] args)
         {
@@ -34,11 +40,13 @@ namespace IdxPart
             }
         }
 
-        private static string GetSanitizedValue(string keyName) => 
+        private static string GetSanitizedValue(string keyName) =>
             keyName.Contains("ApiKey") || keyName.Contains("ConnectionString") ? "..." : ConfigurationManager.AppSettings[keyName];
 
         public static void Main1(string[] args)
         {
+            ServicePointManager.DefaultConnectionLimit = 100;
+
             if (args.Length == 0)
             {
                 var parameters = string.Join("\n              ", ConfigurationManager.AppSettings.AllKeys.Select(k => "/" + k + ":" + GetSanitizedValue(k)));
@@ -54,6 +62,7 @@ USAGE:  idxpart  [instances] action [action] [/Parameter:Value]
               disable
               run
               status
+              import
 
   parameters: Values may be set in config or overwriten via commandline
               " + parameters + @"
@@ -65,7 +74,8 @@ EXAMPLES:
     idxpart status
     idxpart disable 2 5-6
     idxpart enable /SkipValidation:true     
-    idxpart create enable /DatasourceTemplate:datasource-incremental.json /PartitionCount:1
+    idxpart create enable /DatasourceTemplate:datasource-incremental-cosmos.json /PartitionCount:1
+    idxpart import
 
 ", parameters);
                 return;
@@ -74,15 +84,15 @@ EXAMPLES:
             OverrideCongifWithArguments(args);
 
             string searchServiceName = ConfigurationManager.AppSettings["SearchServiceName"];
-            string apiKey = ConfigurationManager.AppSettings["SearchServiceApiKey"];
+            _apiKey = ConfigurationManager.AppSettings["SearchServiceApiKey"];
 
             indexerTemplate = File.ReadAllText(ConfigurationManager.AppSettings["IndexerTemplate"]);
             datasourceTemplate = File.ReadAllText(ConfigurationManager.AppSettings["DatasourceTemplate"]);
 
-            _searchClient = new SearchServiceClient(searchServiceName, new SearchCredentials(apiKey));
+            _searchClient = new SearchServiceClient(searchServiceName, new SearchCredentials(_apiKey));
 
-            _httpClient.DefaultRequestHeaders.Add("api-key", apiKey);
-            
+            _httpClient.DefaultRequestHeaders.Add("api-key", _apiKey);
+
             _searchServiceEndpoint = String.Format("https://{0}.{1}", searchServiceName, _searchClient.SearchDnsSuffix);
 
             bool result = RunAsync(args).GetAwaiter().GetResult();
@@ -157,21 +167,26 @@ EXAMPLES:
 
             Console.WriteLine("Indexer Partitions: {0}", instances);
 
-            DateTimeOffset partitionStartDate;
-            DateTimeOffset partitionEndDate;
-            if (!DateTimeOffset.TryParse(ConfigurationManager.AppSettings["PartitionStartDate"], out partitionStartDate))
+            // use numeric partitioning by default
+            Func<object, object> getNextPartitionValue = (object value) => (int)value + 1;
+            Func<object, string> formatPartitionValue = (object value) => ((int)value).ToString(ConfigurationManager.AppSettings["PartitionFormatString"] ?? "0");
+            object partitionStartValue = 1;
+            object partitionEndValue;
+
+            // use a date time partitioner if Partition Dates are used
+            if (!string.IsNullOrWhiteSpace(ConfigurationManager.AppSettings["PartitionStartDate"]) && !string.IsNullOrWhiteSpace(ConfigurationManager.AppSettings["PartitionEndDate"]))
             {
-                partitionStartDate = DateTimeOffset.FromUnixTimeSeconds(long.Parse(ConfigurationManager.AppSettings["PartitionStartDate"]));
-            }
-            if (!DateTimeOffset.TryParse(ConfigurationManager.AppSettings["PartitionEndDate"], out partitionEndDate))
-            {
-                partitionEndDate = DateTimeOffset.FromUnixTimeSeconds(long.Parse(ConfigurationManager.AppSettings["PartitionEndDate"]));
+                DateTimeOffset partitionStartDate, partitionEndDate;
+                if (!DateTimeOffset.TryParse(ConfigurationManager.AppSettings["PartitionStartDate"], out partitionStartDate))
+                    partitionStartDate = DateTimeOffset.FromUnixTimeSeconds(long.Parse(ConfigurationManager.AppSettings["PartitionStartDate"]));
+                if (!DateTimeOffset.TryParse(ConfigurationManager.AppSettings["PartitionEndDate"], out partitionEndDate))
+                    partitionEndDate = DateTimeOffset.FromUnixTimeSeconds(long.Parse(ConfigurationManager.AppSettings["PartitionEndDate"]));
+
+                TimeSpan interval = TimeSpan.FromTicks((long)((partitionEndDate - partitionStartDate).Ticks / (double)instances));
+                getNextPartitionValue = (object value) => (DateTimeOffset)value + interval;
+                formatPartitionValue = (object value) => ((DateTimeOffset)value).ToUnixTimeSeconds().ToString();
             }
 
-            TimeSpan interval = TimeSpan.FromTicks((long)((partitionEndDate - partitionStartDate).Ticks / (double)instances));
-            
-            DateTimeOffset partitionStartValue = partitionStartDate;
-            DateTimeOffset partitionEndValue;
             for (int i = 1; i <= instances; i++)
             {
                 if (instanceFilter.Any() && !instanceFilter.Contains(i))
@@ -179,11 +194,11 @@ EXAMPLES:
                     continue;
                 }
 
-                partitionEndValue = partitionStartValue + interval;
+                partitionEndValue = getNextPartitionValue(partitionStartValue);
                 string suffix = instances > 1 ? "-" + instances.ToString("000") + "-" + i.ToString("000") : string.Empty;
                 string iName = ConfigurationManager.AppSettings["IndexerNamePrefix"] + suffix;
                 string dsName = string.IsNullOrEmpty(ConfigurationManager.AppSettings["DatasourceNamePrefix"]) ? iName : ConfigurationManager.AppSettings["DatasourceNamePrefix"] + suffix;
-                string description = string.Format("Partition {0} of {1} from {2} to {3} ({4}-{5})", i, instances, partitionStartValue, partitionEndValue, partitionStartValue.ToUnixTimeSeconds(), partitionEndValue.ToUnixTimeSeconds());
+                string description = string.Format("Partition {0} of {1} from {2} to {3} ({4}-{5})", i, instances, partitionStartValue, partitionEndValue, formatPartitionValue(partitionStartValue), formatPartitionValue(partitionEndValue));
 
                 if (args.Contains("delete", StringComparer.InvariantCultureIgnoreCase) || args.Contains("del", StringComparer.InvariantCultureIgnoreCase))
                 {
@@ -196,7 +211,7 @@ EXAMPLES:
                 {
                     Console.WriteLine();
                     Console.WriteLine(description);
-                    result = await CreateDataSource(dsName, description, partitionStartValue.ToUnixTimeSeconds(), partitionEndValue.ToUnixTimeSeconds());
+                    result = await CreateDataSource(dsName, description, formatPartitionValue(partitionStartValue), formatPartitionValue(partitionEndValue));
                     if (!result)
                         return result;
 
@@ -232,7 +247,6 @@ EXAMPLES:
                         Console.WriteLine("Could not run " + iName);
                 }
 
-                
                 if (args.Contains("status", StringComparer.InvariantCultureIgnoreCase) || args.Contains("stats", StringComparer.InvariantCultureIgnoreCase) || args.Contains("stat", StringComparer.InvariantCultureIgnoreCase))
                 {
 
@@ -302,6 +316,13 @@ EXAMPLES:
 
                 partitionStartValue = partitionEndValue;
             }
+
+
+            if (args.Contains("import", StringComparer.InvariantCultureIgnoreCase))
+            {
+                await ImportData(instances);
+            }
+            
             return result;
         }
 
@@ -328,10 +349,11 @@ EXAMPLES:
                 json = json.Replace("[IndexerName]", name);
                 json = json.Replace("[DataSourceName]", dsName);
                 json = json.Replace("[Disabled]", disabled.ToString().ToLowerInvariant());
+                json = json.Replace("[BatchSize]", ConfigurationManager.AppSettings["BatchSize"]);
                 json = json.Replace("[IndexName]", ConfigurationManager.AppSettings["IndexName"]);
                 json = json.Replace("[Description]", description ?? "");
                 string skipValidation = !disabled && bool.Parse(ConfigurationManager.AppSettings["SkipValidation"]) ? "&skipvalidation" : "";
-                string uri = String.Format("{0}/indexers/{1}?api-version=2017-11-11{2}", _searchServiceEndpoint, name, skipValidation);
+                string uri = String.Format("{0}/indexers/{1}?api-version=2019-05-06{2}", _searchServiceEndpoint, name, skipValidation);
                 HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
                 HttpResponseMessage response = await _httpClient.PutAsync(uri, content);
                 if (!response.IsSuccessStatusCode)
@@ -357,11 +379,11 @@ EXAMPLES:
                 string json = datasourceTemplate;
                 json = json.Replace("[DataSourceName]", name);
                 json = json.Replace("[Description]", description ?? "");
-                json = json.Replace("[CosmosConnectionString]", ConfigurationManager.AppSettings["CosmosConnectionString"]);
-                json = json.Replace("[CosmosContainerName]", ConfigurationManager.AppSettings["CosmosContainerName"]);
+                json = json.Replace("[ConnectionString]", ConfigurationManager.AppSettings["ConnectionString"]);
+                json = json.Replace("[ContainerName]", ConfigurationManager.AppSettings["ContainerName"]);
                 json = json.Replace("[ParitionStart]", startValue.ToString());
                 json = json.Replace("[ParitionEnd]", endValue.ToString());
-                string uri = String.Format("{0}/datasources/{1}?api-version=2017-11-11", _searchServiceEndpoint, name);
+                string uri = String.Format("{0}/datasources/{1}?api-version=2019-05-06", _searchServiceEndpoint, name);
                 HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
                 HttpResponseMessage response = await _httpClient.PutAsync(uri, content);
                 if (!response.IsSuccessStatusCode)
@@ -384,7 +406,7 @@ EXAMPLES:
             Console.WriteLine("Reseting Indexer...");
             try
             {
-                string uri = String.Format("{0}/indexers/{1}/reset?api-version=2017-11-11-Preview", _searchServiceEndpoint, name);
+                string uri = String.Format("{0}/indexers/{1}/reset?api-version=2019-05-06-Preview", _searchServiceEndpoint, name);
                 HttpResponseMessage response = await _httpClient.PostAsync(uri, new StringContent(string.Empty));
                 if (!response.IsSuccessStatusCode)
                 {
@@ -406,7 +428,7 @@ EXAMPLES:
             Console.WriteLine("Runing Indexer...");
             try
             {
-                string uri = String.Format("{0}/indexers/{1}/run?api-version=2017-11-11-Preview", _searchServiceEndpoint, name);
+                string uri = String.Format("{0}/indexers/{1}/run?api-version=2019-05-06-Preview", _searchServiceEndpoint, name);
                 HttpResponseMessage response = await _httpClient.PostAsync(uri, new StringContent(string.Empty));
                 if (!response.IsSuccessStatusCode)
                 {
@@ -433,9 +455,9 @@ EXAMPLES:
             Console.WriteLine("Deleting {0} {1}", collection, name);
             try
             {
-                string uri = String.Format("{0}/{1}/{2}?api-version=2017-11-11-Preview", _searchServiceEndpoint, collection, name);
+                string uri = String.Format("{0}/{1}/{2}?api-version=2019-05-06-Preview", _searchServiceEndpoint, collection, name);
                 HttpResponseMessage response = await _httpClient.DeleteAsync(uri);
-                    
+
                 if (!response.IsSuccessStatusCode && response.StatusCode != HttpStatusCode.NotFound)
                 {
                     string responseText = await response.Content.ReadAsStringAsync();
@@ -450,5 +472,148 @@ EXAMPLES:
             }
             return true;
         }
+
+        
+        private static async Task ImportData(int degreeOfParallelism)
+        {
+
+            Console.WriteLine("Importing Data...");
+            string fileName = ConfigurationManager.AppSettings["ImportFile"];
+            string indexName = ConfigurationManager.AppSettings["IndexName"];
+            int batchSize = int.Parse(ConfigurationManager.AppSettings["BatchSize"] ?? "1000");
+
+            // open the file
+            using (var stream = File.OpenRead(fileName))
+            {
+                List<Task> tasks = new List<Task>();
+
+                // read  the stream into a blocking collection to synronize the single reader with the many writer
+                var lines = ReadLines(stream);
+                BlockingCollection<(string,int)[]> docs = new BlockingCollection<(string, int)[]>(degreeOfParallelism * 2);
+                tasks.Add(Task.Run(() => {
+                    List<(string, int)> batch = new List<(string, int)>(batchSize);
+                    foreach (var line in lines.Select((line, lineNumber) => (line, lineNumber + 1)))
+                    {
+                        batch.Add(line);
+                        if (batch.Count == batchSize)
+                        {
+                            docs.Add(batch.ToArray());
+                            batch.Clear();
+                        }
+                    }
+                    if (batch.Count > 0)
+                        docs.Add(batch.ToArray());
+                    docs.CompleteAdding();
+                }));
+
+                // Create the import tasks
+                for (int i = 0; i < degreeOfParallelism; i++)
+                {
+                    int indexIndex = i; // local assignment needed for the async closure to work properly
+                    tasks.Add(Task.Run(() => ImportIntoIndex(docs, indexName, indexIndex)));
+                }
+
+                // monitor the import and adjust the 
+                Stopwatch timer = Stopwatch.StartNew();
+                tasks.Add(Task.Run(async () => {
+                    var runTimer = Stopwatch.StartNew();
+                    while (!docs.IsCompleted)
+                    {
+                        await Task.Delay(TimeSpan.FromSeconds(1));
+                        Console.WriteLine("Elapsed: {0}  DocCount = {1}  AvgInsertMS = {2}  MDocsPerHour = {3:0.00}",
+                            timer.Elapsed,
+                            batchSize * _batchCounter,
+                            _batchCounter > 0 ? Math.Round((double)_durationMS / _batchCounter) : 0,
+                            _batchCounter * batchSize / 1000000.0 / (runTimer.Elapsed.TotalMinutes / 60)
+                         );
+                    }
+                }));
+
+                await Task.WhenAll(tasks);
+            }
+        }
+
+        private static async Task ImportIntoIndex(BlockingCollection<(string, int)[]> batches, string indexName, int indexerIndex)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("api-key", _apiKey);
+            // For telemetry correlation purposes
+            httpClient.DefaultRequestHeaders.Add("client-request-id", Guid.NewGuid().ToString());
+            httpClient.DefaultRequestHeaders.UserAgent.Add(new System.Net.Http.Headers.ProductInfoHeaderValue("idxpart", "1.0"));
+
+            int ramp = int.Parse(ConfigurationManager.AppSettings["RampMinutes"] ?? "0");
+
+            // ramp indexer if desired
+            await Task.Delay(TimeSpan.FromMinutes(ramp * indexerIndex));
+
+            // In order to optimize for performance and isolate all other variables use the most low tech way to insert the data into the index as possible.
+            // we just tweek the json to put it in a batch, add and id and action, and then post it to the rest api
+            StringBuilder jsonPayload = new StringBuilder();
+            while (!batches.IsCompleted)
+            {
+                var batch = batches.Take();
+                if (batch == null)
+                {
+                    break;
+                }
+
+                try
+                {
+                    string uri = String.Format("{0}/indexes/{1}/docs/index?api-version=2019-05-06-Preview", _searchServiceEndpoint, indexName);
+
+                    jsonPayload.Clear();
+                    jsonPayload.Append("{\"value\":[");
+                    bool isFirst = true;
+                    foreach ((string line, int lineNumber) doc in batch)
+                    {
+                        if (isFirst)
+                            isFirst = false;
+                        else
+                            jsonPayload.Append(",");
+
+                        jsonPayload.Append("{\"@search.action\":\"mergeOrUpload\",\"" + (ConfigurationManager.AppSettings["KeyField"] ?? "id") + "\":\"");
+                        jsonPayload.Append(doc.lineNumber);
+                        jsonPayload.Append("\",");
+                        jsonPayload.Append(doc.line.Substring(1, doc.line.Length - 1));
+                    }
+                    jsonPayload.Append("]}");
+
+                    Stopwatch callTime = Stopwatch.StartNew();
+                    HttpResponseMessage response = await httpClient.PostAsync(uri, new StringContent(jsonPayload.ToString(), Encoding.UTF8, "application/json"));
+                    callTime.Stop();
+                    Interlocked.Add(ref _durationMS, callTime.ElapsedMilliseconds);
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        string responseText = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine("Insert Index data response: \n{0}", responseText);
+                    }
+                    else
+                    {
+                        Interlocked.Increment(ref _batchCounter);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error inserting data into index: {0}", ex.Message);
+                }
+            }
+        }
+
+        private static IEnumerable<string> ReadLines(Stream s)
+        {
+            using (var reader = new StreamReader(s))
+            {
+                do
+                {
+                    while (reader.Peek() >= 0)
+                    {
+                        yield return reader.ReadLine();
+                    }
+                    s.Position = 0;
+                }
+                while (ConfigurationManager.AppSettings["Forever"] == "true");
+            }
+        }
+
     }
 }
